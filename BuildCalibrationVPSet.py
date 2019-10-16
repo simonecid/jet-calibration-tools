da029619d6f3d08ebe209d7abe8bb1297d461978
Simone Bologna <smnbologna@gmail.com>
2019-10-16 17:18:43 +0100

    added fit and method to manually smooth out fluctuations

--- BuildCalibrationVPSet.py
+++ BuildCalibrationVPSet.py
@@ -4,9 +4,9 @@ import os
 import pickle as pkl
 from sys import argv
 
-def BuildCalibrationVPSet(rootfile_path, etaBinning, outputfile_path):
+def BuildCalibrationVPSet(rootfile_path, etaBinning, hfBinning, outputfile_path):
 
-  from ROOT import TFile
+  from ROOT import TFile, TCanvas, TF1
   parameters = cms.VPSet()
   
   tfile = TFile(rootfile_path)
@@ -18,18 +18,68 @@ def BuildCalibrationVPSet(rootfile_path, etaBinning, outputfile_path):
     parameter = cms.PSet()
     
     graph = tfile.Get("l1corr_eta_" + str(etaBinning[binIdx]) + "_" + str(etaBinning[binIdx + 1]))
+
+    linear1 = TF1("linear1", "pol1", 80, 200)
+    linear2 = TF1("linear2", "pol1", 200, 600)
+    quad = TF1("quad", "pol2", 30, 80)
+    fit_linear1 = graph.Fit("linear1", "Q", "", 80, 200)
+    fit_linear2 = graph.Fit("linear2", "Q", "", 200, 600)
+    fit_quad = graph.Fit("quad", "Q", "", 30, 80)
     
     if graph == None:
       raise ValueError("Object " + "l1corr_" + str(etaBinning[binIdx]) + "_" + str(etaBinning[binIdx + 1]) + " not found in file " + rootfile_path)
     
     l1tPtCentres = [pt for pt in graph.GetX()]
+    l1tCalibrationFactorPairs = []
+    for index, pt in  enumerate(l1tPtCentres):
+      y = quad.Eval(30) if pt < 30 else quad.Eval(pt) if pt < 80 else linear1.Eval(pt) if pt < 200 else linear2.Eval(pt) 
+      if y <= 0:
+        y = 1
+      if y > 2:
+        print("### WARNING: CALIBRATION FACTOR > 2, ARE YOU SURE EVERYTHING IS OK?")
+      graph.SetPoint(index, pt, y)
+      l1tCalibrationFactorPairs.append((pt, y))
 
-    parameter.l1tPtBins = cms.vdouble([float("-inf")] + [(l1tPtCentres[i] + l1tPtCentres[i+1])/2. for i in xrange(0, len(l1tPtCentres)-1)] + [float("+inf")])
-    parameter.l1tCalibrationFactors = cms.vdouble([y for y in graph.GetY()])
+    l1tCalibrationFactorPairs = sorted(l1tCalibrationFactorPairs)
+    parameter.l1tPtBins = cms.vdouble([float("-inf")] + 
+      [(l1tCalibrationFactorPairs[i][0] + l1tCalibrationFactorPairs[i+1][0])/2. for i in xrange(0, len(l1tCalibrationFactorPairs)-1)] +
+      [float("+inf")])
+    parameter.l1tCalibrationFactors = cms.vdouble([l1tCalibrationFactorPair[1] for l1tCalibrationFactorPair in l1tCalibrationFactorPairs])
     parameter.etaMin = cms.double(etaBinning[binIdx])
     parameter.etaMax = cms.double(etaBinning[binIdx + 1])
-    
+
     parameters.append(parameter)
+
+    canvas = TCanvas()
+
+    graph.GetYaxis().SetRangeUser(0, 3)
+    # graph.GetXaxis().SetRangeUser(0, 200)
+    graph.Draw("APE")
+    linear2.Draw("SAME")
+    linear1.Draw("SAME")
+    quad.Draw("SAME")
+
+    canvas.SaveAs("l1corr_" + str(etaBinning[binIdx]) + "_" + str(etaBinning[binIdx + 1]) + ".png")
+
+  for binIdx in xrange(0, len(hfBinning) -1 ):
+    graph = tfile.Get("l1corr_eta_" + str(hfBinning[binIdx]) + "_" + str(hfBinning[binIdx + 1]))
+    l1tPtCentres = [pt for pt in graph.GetX()]
+
+    l1tCalibrationFactorPairs = []
+    for index, pt in  enumerate(l1tPtCentres):
+      y = 1
+      graph.SetPoint(index, pt, y)
+      l1tCalibrationFactorPairs.append((pt, y))
+
+    l1tCalibrationFactorPairs = sorted(l1tCalibrationFactorPairs)
+    parameter = cms.PSet()
+    parameter.etaMin = cms.double(hfBinning[binIdx])
+    parameter.etaMax = cms.double(hfBinning[binIdx + 1])
+    parameter.l1tPtBins = cms.vdouble([float("-inf")] + [(l1tPtCentres[i] + l1tPtCentres[i+1])/2. for i in xrange(0, len(l1tPtCentres)-1)] + [float("+inf")])
+    parameter.l1tCalibrationFactors = cms.vdouble([y for y in graph.GetY()])
+    parameters.append(parameter)
+
+
 
   tfile.Close()
 
@@ -41,7 +91,23 @@ def BuildCalibrationVPSet(rootfile_path, etaBinning, outputfile_path):
 
 
 if __name__ == "__main__":
-  BuildCalibrationVPSet("/hdfs/user/sb17498/CMS_Phase_2/jetMETStudies/CalibrationFactors/Calibration_TTBar_PU200_Jets_Histograms_CalibrationTree_MatchAK4GenJetWithPhase1L1TJetFromPfCandidates_3766735.0.root",
-  [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964, 3.489, 4.191, 5.191],
-  "Calibration_TTBar_PU200_Jets_Histograms_CalibrationTree_MatchAK4GenJetWithPhase1L1TJetFromPfCandidates_3766735.0.py"
+  BuildCalibrationVPSet("ComputeUncalibratedPhase1AndAK4L1TJetsFromPfCandidates_10_0_4_MTD_5x5Jets_CalibrationFactors_MatchAK4GenJetWithAK4JetFromPfInputs.root",
+  [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964],
+  [2.964, 3.489, 4.191, 5.191],
+  "ComputeUncalibratedPhase1AndAK4L1TJetsFromPfCandidates_10_0_4_MTD_5x5Jets_CalibrationFactors_MatchAK4GenJetWithAK4JetFromPfInputs.py"
   )
+  BuildCalibrationVPSet("ComputeUncalibratedPhase1AndAK4L1TJetsFromPfCandidates_10_0_4_MTD_5x5Jets_CalibrationFactors_MatchAK4GenJetWithPhase1L1TJetFromPfInputs.root",
+  [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964],
+  [2.964, 3.489, 4.191, 5.191],
+  "ComputeUncalibratedPhase1AndAK4L1TJetsFromPfCandidates_10_0_4_MTD_5x5Jets_CalibrationFactors_MatchAK4GenJetWithPhase1L1TJetFromPfInputs.py"
+  )
+  # BuildCalibrationVPSet("ComputeUncalibratedPhase1AndAK4L1TJetsFromPfCandidates_10_0_4_MTD_CalibrationFactors_MatchAK4GenJetWithAK4JetFromPfInputs.root",
+  # [],
+  # [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964, 3.489, 4.191, 5.191],
+  # "Calibration_1.py"
+  # )
+  # BuildCalibrationVPSet("ComputeUncalibratedPhase1AndAK4L1TJetsFromPfCandidates_10_0_4_MTD_9x9Jets_CalibrationFactors_MatchAK4GenJetWithPhase1L1TJetFromPfInputs.root",
+  # [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964],
+  # [2.964, 3.489, 4.191, 5.191],
+  # "ComputeUncalibratedPhase1AndAK4L1TJetsFromPfCandidates_10_0_4_MTD_9x9Jets_CalibrationFactors_MatchAK4GenJetWithPhase1L1TJetFromPfInputs.py"
+  # )
